7e889bb6ecd25d474f0d86ed02432ed8d2188f43
Thomas ZILLIOX <thomas@zilliox.me>
2013-05-20 16:38:40 +0200

    Add page down & up support

--- bespoke.js
+++ bespoke.js
@@ -1,2 +1,207 @@
-/*! Bespoke.js v0.1.0 © 2013 Mark Dalgleish, Licensed MIT */
-(function(e,t,n){var r=function(e,t){var r=n.querySelector(e),s=[].slice.call(r.children,0),c=s[0],h={},p=function(e){if(!s[e])return;a(h,"deactivate",{slide:c,index:s.indexOf(c)}),c=s[e],s.map(v),a(h,"activate",{slide:c,index:e}),f(c,"active"),l(c,"inactive")},v=function(e,t){var n=t-s.indexOf(c),r=n>0?"after":"before";["before(-\\d+)?","after(-\\d+)?","active","inactive"].map(l.bind(null,e)),e!==c&&["inactive",r,r+"-"+Math.abs(n)].map(f.bind(null,e))},m=function(e){a(h,"slide",{slide:s[e],index:e})&&p(e)},g=function(){var e=s.indexOf(c)+1;a(h,"next",{slide:c,index:s.indexOf(c)})&&p(e)},y=function(){var e=s.indexOf(c)-1;a(h,"prev",{slide:c,index:s.indexOf(c)})&&p(e)},b={on:o.bind(null,h),off:u.bind(null,h),fire:a.bind(null,h),slide:m,next:g,prev:y,parent:r,slides:s};return p(0),f(r,"parent"),s.map(function(e){f(e,"slide")}),Object.keys(t||{}).map(function(e){var n=t[e];n&&d[e](b,n===!0?{}:n)}),i.push(b),b},i=[],s={},o=function(e,t,n){(e[t]||(e[t]=[])).push(n)},u=function(e,t,n){e[t]=(e[t]||[]).filter(function(e){return e!==n})},a=function(e,t,n){return(e[t]||[]).concat(e!==s&&s[t]||[]).reduce(function(e,t){return e&&t(n)!==!1},!0)},f=function(t,n){t.classList.add(e+"-"+n)},l=function(t,n){t.className=t.className.replace(new RegExp(e+"-"+n+"(\\s|$)","g")," ").replace(/^\s+|\s+$/g,"")},c=function(e){return function(t){i.map(function(n){n[e].call(null,t)})}},h=function(e){return{from:function(t,n){return(n=n||{})[e]=!0,r(t,n)}}},p=function(e){return function(t){var r,i;n.addEventListener("keydown",function(n){var r=n.which;e==="X"?(r===37&&t.prev(),(r===32||r===39)&&t.next()):(r===38&&t.prev(),(r===32||r===40)&&t.next())}),t.parent.addEventListener("touchstart",function(t){t.touches.length&&(r=t.touches[0]["page"+e],i=0)}),t.parent.addEventListener("touchmove",function(t){t.touches.length&&(t.preventDefault(),i=t.touches[0]["page"+e]-r)}),t.parent.addEventListener("touchend",function(){Math.abs(i)>50&&(i>0?t.prev():t.next())})}},d={horizontal:p("X"),vertical:p("Y")};t[e]={from:r,slide:c("slide"),next:c("next"),prev:c("prev"),horizontal:h("horizontal"),vertical:h("vertical"),on:o.bind(null,s),off:u.bind(null,s),plugins:d}})("bespoke",this,document);
+(function(moduleName, window, document) {
+    var from = function(selector, selectedPlugins) {
+          var parent = document.querySelector(selector),
+            slides = [].slice.call(parent.children, 0),
+            activeSlide = slides[0],
+            deckListeners = {},
+
+            activate = function(index, customData) {
+                if (!slides[index]) {
+                    return;
+                }
+
+                fire(deckListeners, 'deactivate', createEventData(activeSlide, customData));
+
+                activeSlide = slides[index];
+
+                slides.map(deactivate);
+
+                fire(deckListeners, 'activate', createEventData(activeSlide, customData));
+
+                addClass(activeSlide, 'active');
+                removeClass(activeSlide, 'inactive');
+            },
+
+            deactivate = function(slide, index) {
+                var offset = index - slides.indexOf(activeSlide),
+                  offsetClass = offset > 0 ? 'after' : 'before';
+
+                ['before(-\\d+)?', 'after(-\\d+)?', 'active', 'inactive'].map(removeClass.bind(null, slide));
+
+                slide !== activeSlide &&
+                ['inactive', offsetClass, offsetClass + '-' + Math.abs(offset)].map(addClass.bind(null, slide));
+            },
+
+            slide = function(index, customData) {
+                fire(deckListeners, 'slide', createEventData(slides[index], customData)) && activate(index, customData);
+            },
+
+            next = function(customData) {
+                var nextSlideIndex = slides.indexOf(activeSlide) + 1;
+
+                fire(deckListeners, 'next', createEventData(activeSlide, customData)) && activate(nextSlideIndex, customData);
+            },
+
+            prev = function(customData) {
+                var prevSlideIndex = slides.indexOf(activeSlide) - 1;
+
+                fire(deckListeners, 'prev', createEventData(activeSlide, customData)) && activate(prevSlideIndex, customData);
+            },
+
+            createEventData = function(slide, eventData) {
+                eventData = eventData || {};
+                eventData.index = slides.indexOf(slide);
+                eventData.slide = slide;
+                return eventData;
+            },
+
+            deck = {
+                on: on.bind(null, deckListeners),
+                off: off.bind(null, deckListeners),
+                fire: fire.bind(null, deckListeners),
+                slide: slide,
+                next: next,
+                prev: prev,
+                parent: parent,
+                slides: slides
+            };
+
+          addClass(parent, 'parent');
+
+          slides.map(function(slide) {
+              addClass(slide, 'slide');
+          });
+
+          Object.keys(selectedPlugins || {}).map(function(pluginName) {
+              var config = selectedPlugins[pluginName];
+              config && plugins[pluginName](deck, config === true ? {} : config);
+          });
+
+          activate(0);
+
+          decks.push(deck);
+
+          return deck;
+      },
+
+      decks = [],
+
+      bespokeListeners = {},
+
+      on = function(listeners, eventName, callback) {
+          (listeners[eventName] || (listeners[eventName] = [])).push(callback);
+      },
+
+      off = function(listeners, eventName, callback) {
+          listeners[eventName] = (listeners[eventName] || []).filter(function(listener) {
+              return listener !== callback;
+          });
+      },
+
+      fire = function(listeners, eventName, eventData) {
+          return (listeners[eventName] || [])
+            .concat((listeners !== bespokeListeners && bespokeListeners[eventName]) || [])
+            .reduce(function(notCancelled, callback) {
+                return notCancelled && callback(eventData) !== false;
+            }, true);
+      },
+
+      addClass = function(el, cls) {
+          el.classList.add(moduleName + '-' + cls);
+      },
+
+      removeClass = function(el, cls) {
+          el.className = el.className
+            .replace(new RegExp(moduleName + '-' + cls +'(\\s|$)', 'g'), ' ')
+            .replace(/^\s+|\s+$/g, '');
+      },
+
+      callOnAllInstances = function(method) {
+          return function(arg) {
+              decks.map(function(deck) {
+                  deck[method].call(null, arg);
+              });
+          };
+      },
+
+      bindPlugin = function(pluginName) {
+          return {
+              from: function(selector, selectedPlugins) {
+                  (selectedPlugins = selectedPlugins || {})[pluginName] = true;
+                  return from(selector, selectedPlugins);
+              }
+          };
+      },
+
+      makePluginForAxis = function(axis) {
+          return function(deck) {
+              var startPosition,
+                delta,
+                keys = {
+                    SPACE: 32,
+                    PAGE_UP: 33,
+                    PAGE_DOWN: 34,
+                    LEFT: 37,
+                    TOP: 38,
+                    RIGHT: 39,
+                    BOTTOM: 40
+                }
+
+              document.addEventListener('keydown', function(e) {
+                  var key = e.which;
+                  if (
+                    key === keys.PAGE_UP ||
+                      (e.shiftKey && key === keys.SPACE) ||
+                      (axis === 'X' && key === keys.LEFT) ||
+                      (axis === 'Y' && key === keys.TOP)
+
+                    ) {
+                      deck.prev();
+                  } else if (
+                    key === keys.PAGE_DOWN ||
+                      key === keys.SPACE ||
+                      (axis === 'X' && key === keys.RIGHT) ||
+                      (axis === 'Y' && key === keys.BOTTOM)
+                    ) {
+                      deck.next();
+                  }
+              });
+
+              deck.parent.addEventListener('touchstart', function(e) {
+                  if (e.touches.length) {
+                      startPosition = e.touches[0]['page' + axis];
+                      delta = 0;
+                  }
+              });
+
+              deck.parent.addEventListener('touchmove', function(e) {
+                  if (e.touches.length) {
+                      e.preventDefault();
+                      delta = e.touches[0]['page' + axis] - startPosition;
+                  }
+              });
+
+              deck.parent.addEventListener('touchend', function() {
+                  Math.abs(delta) > 50 && (delta > 0 ? deck.prev() : deck.next());
+              });
+          };
+      },
+
+      plugins = {
+          horizontal: makePluginForAxis('X'),
+          vertical: makePluginForAxis('Y')
+      };
+
+    window[moduleName] = {
+        from: from,
+        slide: callOnAllInstances('slide'),
+        next: callOnAllInstances('next'),
+        prev: callOnAllInstances('prev'),
+        horizontal: bindPlugin('horizontal'),
+        vertical: bindPlugin('vertical'),
+        on: on.bind(null, bespokeListeners),
+        off: off.bind(null, bespokeListeners),
+        plugins: plugins
+    };
+
+}('bespoke', this, document));
